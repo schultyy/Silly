d89a43a8c686b4112c8abc1d6ceae86dcbfc91e5
Jan Schulte <hello@unexpected-co.de>
2015-05-17 00:09:21 +0200

    add shell project

---
+++ Silly.Shell/Compiler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ClearScript.V8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silly.Shell
+{
+    public class Compiler
+    {
+        public void Compile(string sourceFile)
+        {
+            // create a script engine
+            using (var engine = new V8ScriptEngine())
+            {
+
+            }
+        }
+    }
+}
