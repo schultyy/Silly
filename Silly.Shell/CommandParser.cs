ff5c72a6f4889c079ebba0e3daa71af1e2db395a
Jan Schulte <hello@unexpected-co.de>
2015-05-17 00:22:18 +0200

    Add commandparser

---
+++ Silly.Shell/CommandParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silly.Shell
+{
+    public class CommandParser
+    {
+        public CommandParser()
+        {
+
+        }
+
+        public string[] Parse(string command)
+        {
+            if(command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+            if(command == string.Empty)
+            {
+                throw new ArgumentException("command");
+            }
+
+            if(command.Contains(" "))
+            {
+                return command.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            return new[] { command };
+        }
+    }
+}
