5383d4b758824467ae3957ac97b7fbd36588de41
Jan Schulte <hello@unexpected-co.de>
2015-05-17 22:30:43 +0200

    add pwd command

---
+++ Silly.Shell/Commands/pwd.ts
@@ -0,0 +1,12 @@
+﻿///<reference path="Definitions/environment.d.ts" />
+
+class Pwd {
+    execute(env: Environment) {
+        return env.CurrentWorkingDirectory;
+    }
+}
+
+function call(env) {
+    var ls = new Pwd();
+    return ls.execute(env);
+}
