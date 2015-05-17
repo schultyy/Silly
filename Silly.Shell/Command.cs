421f537993f744fe2cc617b7e907de7c302ccc1d
Jan Schulte <hello@unexpected-co.de>
2015-05-17 18:04:37 +0200

    Command Execute returns result

--- Silly.Shell/Command.cs
+++ Silly.Shell/Command.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ClearScript.V8;
 
 namespace Silly.Shell
 {
-    public class Command
+    public class Command : IDisposable
     {
-        public void Execute(string[] args) { }
+        private V8ScriptEngine scriptEngine;
+
+        private string script;
+
+        public Command(string script)
+        {
+            this.script = script;
+            scriptEngine = new V8ScriptEngine();
+        }
+
+        ~Command()
+        {
+            Dispose();
+        }
+
+        public object Execute(string[] args)
+        {
+            return scriptEngine.Evaluate(this.script);
+        }
+
+        public void Dispose()
+        {
+            scriptEngine.Dispose();
+        }
     }
 }
