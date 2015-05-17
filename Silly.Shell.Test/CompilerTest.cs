c6fb8b94ba424558cb8e148e5a0f43d7ac07169c
Jan Schulte <hello@unexpected-co.de>
2015-05-17 13:53:52 +0200

    add sourcefile as variable in script

---
+++ Silly.Shell.Test/CompilerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Silly.Shell.Test
+{
+    [TestClass]
+    public class CompilerTest
+    {
+        private string testScript = @"
+            class Student {
+                fullname : string;
+                constructor(public firstname, public middleinitial, public lastname) {
+                    this.fullname = firstname + '' + middleinitial + '' + lastname;
+                }
+            }
+            ";
+        private Compiler compiler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            compiler = new Compiler();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaiseExceptionIfScriptIsNull()
+        {
+            compiler.Compile(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RaiseExceptionIfScriptIsEmpty()
+        {
+            compiler.Compile(string.Empty);
+        }
+
+        [TestMethod]
+        public void CompileTestScriptResultIsNotEmpty()
+        {
+            Assert.IsNotNull(compiler.Compile(testScript));
+        }
+    }
+}
--- Silly.Shell/Compiler.cs
+++ Silly.Shell/Compiler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.ClearScript.V8;
+﻿using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,14 @@ namespace Silly.Shell
 {
     public class Compiler
     {
-        public void Compile(string sourceFile)
+        public object Compile(string sourceFile)
         {
-            // create a script engine
             using (var engine = new V8ScriptEngine())
             {
-
+                engine.Script["sourcefile"] = sourceFile;
             }
+            
+            return null;
         }
     }
 }
