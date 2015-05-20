cb4494b91a6131a788fe4a1b6ec6b4d33a164cf4
Jan Schulte <hello@unexpected-co.de>
2015-05-20 18:28:48 +0200

    - improve cd command for better navigation - add definition file for System.Console

--- Silly.Shell/ScriptRuntime.cs
+++ Silly.Shell/ScriptRuntime.cs
@@ -14,7 +14,7 @@ namespace Silly.Shell
         public ScriptRuntime(List<File> commandFiles)
         {
             scriptEngine = new V8ScriptEngine();
-            this.scriptEngine.AddHostType("Console", typeof(Console));
+            this.scriptEngine.AddHostType("ClrConsole", typeof(Console));
             this.scriptEngine.AddHostType("ClrString", typeof(string));
             this.scriptEngine.AddHostType("ClrList", typeof(List<>));
             this.scriptEngine.AddHostType("ClrArray", typeof(Array));
@@ -24,6 +24,7 @@ namespace Silly.Shell
             this.scriptEngine.AddHostType("DirectoryInfo", typeof(System.IO.DirectoryInfo));
             this.scriptEngine.AddHostType("Environment", typeof(Silly.Shell.Environment));
             this.scriptEngine.AddHostType("ClrEnvironment", typeof(System.Environment));
+            this.scriptEngine.AddHostType("ClrPath", typeof(System.IO.Path));
             this.LoadCommands(commandFiles);
         }
 
